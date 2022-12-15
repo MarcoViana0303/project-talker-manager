f94da97a473d781c1229d4df40a0c889d57ac00b
Marco Viana <marcoviana100@gmail.com>
2022-12-15 18:14:44 -0300

    feat: requisito 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ const path = require('path');
 const { fsReadFile } = require('./utils/fsUtils');
 // const talker = require('./talker.json');
 
+const example = [
+  {
+    "name": "Henrique Albuquerque",
+    "age": 62,
+    "id": 1,
+    "talk": {
+      "watchedAt": "23/10/2020",
+      "rate": 5
+    }
+  },
+  {
+    "name": "Heloísa Albuquerque",
+    "age": 67,
+    "id": 2,
+    "talk": {
+      "watchedAt": "23/10/2020",
+      "rate": 5
+    }
+  },
+  {
+    "name": "Ricardo Xavier Filho",
+    "age": 33,
+    "id": 3,
+    "talk": {
+      "watchedAt": "23/10/2020",
+      "rate": 5
+    }
+  },
+  {
+    "name": "Marcos Costa",
+    "age": 24,
+    "id": 4,
+    "talk": {
+      "watchedAt": "23/10/2020",
+      "rate": 5
+    }
+  }
+]
+
 const app = express();
 app.use(express.json());
 
@@ -21,6 +60,19 @@ app.get('/talker', async (_req, res) => {
   return res.status(200).json([]);
 } 
  return res.status(HTTP_OK_STATUS).json(talker);
+});
+
+// Requisito 2
+app.get('/talker/:id', async (req, res) => {
+const { id } = req.params;
+const talker = await fsReadFile(path.resolve(__dirname, './talker.json'));
+const otherExample = talker.find((idTalker) => idTalker.id === +id)
+console.log(otherExample);
+
+if (!otherExample) {
+  return res.status(404).json({ message: "Pessoa palestrante não encontrada"})
+}
+
 });
 
 app.listen(PORT, () => {
